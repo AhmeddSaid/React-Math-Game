bee566fa310bdfd7c47761bac776ce5ada9375ca
Ahmed Said <ahmed_adnan@stud.cu.edu.eg>
2022-09-21 02:58:07 +0200

    Updates

---
+++ precache-manifest.6d6f8645801e62082705e14a42248ab7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "481f21447ff4424ddf2c",
+    "url": "/React-Math-Game.git/static/css/main.57b9f672.chunk.css"
+  },
+  {
+    "revision": "481f21447ff4424ddf2c",
+    "url": "/React-Math-Game.git/static/js/main.481f2144.chunk.js"
+  },
+  {
+    "revision": "a3c0bc647fa7f281b58f",
+    "url": "/React-Math-Game.git/static/js/1.a3c0bc64.chunk.js"
+  },
+  {
+    "revision": "86f6b9692353c3979ba2",
+    "url": "/React-Math-Game.git/static/js/runtime~main.86f6b969.js"
+  },
+  {
+    "revision": "401f2236d6e39a1bb66da67f65f233fe",
+    "url": "/React-Math-Game.git/index.html"
+  }
+];
